de1528f99fcf9659d3fdabc55988c955c0aec73e
oumar <balloumar49@hotmail.com>
2019-05-26 15:06:24 +0200

    Ajout diagramme des classes du TP 3

---
+++ TP3/src/pizza/Main.java
@@ -0,0 +1,18 @@
+package pizza;
+
+public class Main {
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+
+		Pizza p = new Pizza();
+p.bake();
+//		p.bake();
+		p.deliver();
+//		p.deliver();
+	}
+
+}
